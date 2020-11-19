ead53f2458982b346454cbd5e40858a325733052
benazeerturkmani <bena0013@stud.kea.dk>
2020-11-19 14:12:01 +0100

    added the skat year CRUD Need to figure out how to join tables

--- Skat/SKAT_API.py
+++ Skat/SKAT_API.py
@@ -21,9 +21,9 @@ def menu_switch():
     if menu_option == 1:
         User_CRUD_menu()
     elif menu_option == 2:
-        skat_Year_CRUD_menu
+        skat_Year_CRUD_menu()
     elif menu_option == 3:
-        return
+        create_SKAT_USER_YEAR()
     else:
         menu_switch()
 
@@ -136,7 +136,7 @@ def update_skat_user():
         print("")
         CreatedAt = input("CreatedAt: ")
         IsActive = input("isActive: ")
-        ID = input("id: ")
+        ID = input("Enter a USER id to change: ")
         db_cursor.execute(qry, (CreatedAt,IsActive,ID))
         db.commit()
         print("________________________________")
@@ -164,17 +164,98 @@ def delte_skat_user():
 
 # CRUD operations for skat year
 def create_new_skat_year():
-    print("CREATE new YEAR")
+    try:
+        print("")
+        print("CREATE new YEAR")
+        print("You are inserting new skat YEAR ")
+        print("______________________________________________________________")
+        print("Date needed: Label, CreatedAt, ModifiedAt,StartDate, EndDate.")
+        print("All date formating: yyyy-mm-dd")
+        print("")
+        Id = input("Id: ")
+        label = input("label: ")
+        createdAt = input("createdAt: ")
+        modifiedAt = input("modifiedAt: ")
+        startDate = input("startDate: ")
+        enddate = input("enddate: ")
+        print("")
+        query = '''INSERT INTO SKAT_YEAR VALUES(?,?,?,?,?,?)'''
 
+        db_cursor.execute(query, (Id,label, createdAt, modifiedAt, startDate,enddate))
+        db.commit()
+        print("record inserted successfully")
+        #create_SKAT_USER_YEAR()
+        return skat_Year_CRUD_menu()
+
+    except:
+        print("error in operation")
 
 def read_skat_year():
-    print("READ user YEAR")
+    print("READ SKAT YEAR")
+    print("...")
+    retrieve_command = '''SELECT * FROM SKAT_YEAR;'''
+    db_cursor.execute(retrieve_command)
+    try:
+        record=db_cursor.fetchall()
+        for x in record:
+            print(x)
+            print("____________________")
+        return skat_Year_CRUD_menu()
+    except:
+        print("Error")
 
 def update_skat_year():
-    print("UPDATE user YEAR")
+    print("UPDATE SKAT YEAR")
+    qry="UPDATE SKAT_YEAR SET label=?, createdAt=?,modifiedAt=?,startDate=?,enddate=? WHERE id=?;"
+    try:
+        print("")
+        print("REMEMBER")
+        print("ALL DATE formating: yyyy-mm-dd")
+        print("")
+        label = input("label: ")
+        modifiedAt = input("modifiedAt: ")
+        enddate = input("enddate: ")
+        ID = input("Enter a YEAR id to change: ")
+        db_cursor.execute(qry, (label,modifiedAt,enddate,ID))
+        db.commit()
+        print("________________________________")
+        print("record updated successfully")
+        return skat_Year_CRUD_menu()
+    except:
+        print("error in operation")
+        db.rollback()
 
 def delte_skat_year():
-    print("DELETE user YEAR")
+    print("DELETE SKAT YEAR")
+
+    qry="DELETE FROM SKAT_YEAR WHERE id=?;"
+    try:
+        print("Pick a YEAR id")
+        id=input("Enter YEAR id: ")
+        db_cursor.execute(qry, (id,))
+        db.commit()
+        print("________________________________")
+        print("record deleted successfully")
+        return User_CRUD_menu()
+    except:
+        print("error in operation")
+        db.rollback()
+
+# SKAT USER YEAR functions
+# need to be re-done
+def create_SKAT_USER_YEAR():
+    SUY_query = '''SELECT SKAT_USER.Id,SKAT_YEAR.Id, SKAT_USER.UserId
+                FROM SKAT_YEAR, SKAT_USER
+                ON SKAT_YEAR.id=SKAT_USER.Id;'''
+
+    db_cursor.execute(SUY_query)
+    try:
+        record=db_cursor.fetchall()
+        print(record)
+    except:
+        print("error in operation")
+
+
 
 # Tax Calculator Function
 def pay_taxes():
